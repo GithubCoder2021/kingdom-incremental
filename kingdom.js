8110215e22bfbd32620d29a74f4df5c482f22652
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-27 13:37:31 -0500

    Update kingdom.js

--- kingdom.js
+++ kingdom.js
@@ -2,6 +2,8 @@ var game = {
    fruit: 50,
    veggies: 50,
    spoiledFood: 0,
+   water: 10,
+   muddyWater: 0,
    population: 10,
    workforce: 0,
    maxWorkforce: 10,
@@ -12,10 +14,19 @@ var game = {
    children: 0,
    adults: 10,
    elders: 0,
-   year: 0
+   year: 0,
+   happiness: 50,
+   happyMult: 1,
+   birthRate: 1
 }
 
 setInterval (function() {
    game.population = game.babies + game.children + game.adults + game.elders;
    document.getElementById("population").innerText = game.population;
+   game.happyMult = game.happiness/50;
+   document.getElementById("happiness").innerText = game.happiness;
+   if (game.happiness <= 10) {
+      game.happyMult = 0.1;
+   }
 }, 10);
+
