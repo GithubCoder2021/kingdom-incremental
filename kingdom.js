8911e945853ddc3a43b988a4c4e022383568eff9
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-02-06 12:04:57 -0500

    Update kingdom.js

--- kingdom.js
+++ kingdom.js
@@ -53,3 +53,26 @@ function giveJob(job) {
       game.workforce++;
    }
 }
+
+setInterval (function() {
+   
+},50};
+             
+function gameLoop(){
+   game.population = game.babies + game.children + game.adults + game.elders;
+   document.getElementById("population").innerText = game.population;
+   game.happyMult = game.happiness/50;
+   document.getElementById("happiness").innerText = game.happiness;
+   if (game.happiness <= 12) {
+      game.happyMult = 0.25;
+   }
+   if (game.happiness >= 100) {
+      game.happiness = 100;
+   }
+   if (game.happiness <= 0) {
+      game.happiness = 0;
+   }
+   
+};
+
+             
